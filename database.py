b2afba9c1ea7e9ded940c05bfabf35933c91a71f
Aliyu Maigida <maigidaaliyu1@gmail.com>
2026-06-05 12:05:28 +0100

    Update database.py

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "users.db")
 
+
 def get_connection():
     return sqlite3.connect(DB_PATH, check_same_thread=False)
 
